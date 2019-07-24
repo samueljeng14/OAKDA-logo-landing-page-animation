902a8655c3595870bcbbae4fde2b1b27cc50c23e
samueleng14 <samueljengmts@gmail.com>
2019-07-24 11:29:15 +0800

    modify animation

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
 $(document).ready(function() {
-	console.log("test");
+	// console.log("test");
 
 	// 點了飛走（一次性）
 	$(".for_hover").click(function() {
-		console.log("test");
+		// console.log("test");
+
+		// 保持旋轉並放大
 		$(".chat").css({
-			transitionDuration: '0.5s',
-			transform: 'scale(1.2) rotate(45deg)'
+			transitionDuration: '0.4s',
+			transform: 'scale(1.2) rotate(45deg)',
+			// transitionTimingFunction: 'cubic-bezier(0,-0.05,.25,1)'
 		});
 		$(".chat").removeClass('chat_hover');
-		setTimeout(function () {
-			$(".chat").css({
-				transitionDuration: '0.5s',
-				transform: 'scale(1.2) translateX(-40px) rotate(45deg)'
-			});
-		});
+
+		// 往左邊一一點
+		// setTimeout(function () {
+		// 	$(".chat").css({
+		// 		transitionDuration: '0.5s',
+		// 		transform: 'scale(1.2) translateX(-40px) rotate(45deg)'
+		// 	});
+		// });
 
 		setTimeout(function () {
 			// 泡泡往右
 			$(".chat").css({
 				transitionDuration: '0.8s',
-				transform: 'scale(1.2) translateX(2000px) rotate(45deg)'
+				transform: 'scale(1.2) translateX(2000px) rotate(45deg)',
+				transitionTimingFunction: 'cubic-bezier(.67,-0.26,.46,1)'
 			});
 
 			// 左邊蓋版
 			$(".bgc").css('width', '100vw');
-		}, 700);
+		}, 450);
 
 		setTimeout(function () {
 			// 文字出現
 			$(".bgc span").css('opacity', '1');
-		}, 1200);
+		}, 1300);
 
 		// 中間三條線消失
 
